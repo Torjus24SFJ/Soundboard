7014078ddab9bf90764062a96350a66019977e50
Torjus <Torjus24sfj@gmail.com>
2025-02-18 10:00:16 +0100

    fix maybe

--- js/soundboard_oppgave.js
+++ js/soundboard_oppgave.js
@@ -4,7 +4,6 @@ async function loadSounds() {
   try {
     const response = await fetch("../data/Sound-file.json/");
     const sounds = await response.json();
-    console.log(sounds);
     sounds.forEach(createSoundButton);
   } catch (error) {
     console.error("Error fetching sounds:", error);
@@ -14,12 +13,14 @@ async function loadSounds() {
 loadSounds();
 
 const drumkit = document.getElementById("drumkit");
+
 function createSoundButton(sound) {
   const button = document.createElement("button");
-  button.textContent = `${sound.label} (${sound.key})`;
   const randomColor = `hsl(${Math.random() * 360}, 100%, 75%)`;
-  button.style.backgroundColor = randomColor;
   const audio = document.createElement("audio");
+  
+  button.textContent = `${sound.label} (${sound.key})`;
+  button.style.backgroundColor = randomColor;
   audio.src = sound.sound;
   audio.id = sound.key;
   document.addEventListener('keypress', (e) => {
