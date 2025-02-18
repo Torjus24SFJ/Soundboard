bdfece92bcee2e0d79a4940c97f2d8188d6ff6bb
Torjus <Torjus24sfj@gmail.com>
2025-02-18 10:32:13 +0100

    losing hope

--- js/soundboard_oppgave.js
+++ js/soundboard_oppgave.js
@@ -16,24 +16,13 @@ const drumkit = document.getElementById("drumkit");
 
 function createSoundButton(sound) {
   const button = document.createElement("button");
-  button.textContent = `${sound.label} (${sound.key})`;
   const randomColor = `hsl(${Math.random() * 360}, 100%, 75%)`;
-  button.style.backgroundColor = randomColor;
   const audio = document.createElement("audio");
+
+  button.textContent = `${sound.label} (${sound.key})`;
+  button.style.backgroundColor = randomColor;
   audio.src = sound.sound;
   audio.id = sound.key;
-
-  document.addEventListener('keypress', (e) => {
-    if (e.key === sound.key){ 
-      if (currentAudio && currentAudio !== audio) 
-        {
-          currentAudio.pause();
-          currentAudio.currentTime = 0;
-        }
-        audio.play();
-        currentAudio = audio;
-    }
-  })
 
   button.addEventListener("click", () => {
     if(currentAudio){
